c6e047db535a85073db157373d4d875703259df3
calcite <calcitevalentin@outlook.com>
2026-09-24 23:04:27 +0800

    separate 2 server

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://calc1te.github.io/",
+	site: "https://blog.calciiite.icu/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
